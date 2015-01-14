b64a0220da9501f7727be024ecc69aa6171f35a4
Ori <ori.moisis@mail.huji.ac.il>
2015-01-14 19:20:56 +0200

    multiple rays in camera

--- ex5new/camera.cpp
+++ ex5new/camera.cpp
@@ -32,6 +32,8 @@ Camera::Camera(Point3d& pos, Point3d& coi, Vector3d& up, double fov,
 void Camera::render(size_t row_start, size_t number_of_rows, BImage& img,
                     Scene& scene) const {
 
+    srand(0);
+
     double fov_v = this->_fov_h * img.width() / img.height();
 
     // TODO: if something stops working - the fact that colDir and rowDir are opposite
@@ -39,19 +41,35 @@ void Camera::render(size_t row_start, size_t number_of_rows, BImage& img,
 
     for (int i = row_start; i < row_start + number_of_rows; ++i) {
 
-        double rowDir = (0.5 + i - img.height()/2) * tan(fov_v) * 2 / img.height();
-
         for (int j = 0; j < img.width(); ++j) {
+            Color3d color = COLOR_BLACK;
 
-            double colDir = (img.width()/2 - 0.5 - j) * tan(this->_fov_h) * 2 / img.width();
+            for (int k = 0; k < this->_samples_per_pixel; ++k) {
+                double rowOffset = 0.5;
+                double colOffset = 0.5;
 
-            double zDir = this->_coi[2] - this->_position[2];
-            glm::vec4 dir(rowDir, colDir, zDir / abs(zDir), 0);
-            dir = this->_transform * dir;
+                if (this->_samples_per_pixel > 1) {
+                    rowOffset = (double)rand() / RAND_MAX;
+                    colOffset = (double)rand() / RAND_MAX;
+                }
 
-            Ray r (this->_position, Vector3d(dir.x, dir.y, dir.z));
-            std::cout << "Tracing ray at pixel " << j << ',' << i << std::endl;
-            Color3d color = scene.trace_ray(r);
+                double rowDir = (rowOffset + i - img.height()/2) * tan(fov_v) * 2 / img.height();
+                double colDir = (img.width()/2 - colOffset - j) * tan(this->_fov_h) * 2 / img.width();
+
+                double zDir = this->_coi[2] - this->_position[2];
+                glm::vec4 dir(rowDir, colDir, zDir / abs(zDir), 0);
+                dir = this->_transform * dir;
+
+                Ray r (this->_position, Vector3d(dir.x, dir.y, dir.z));
+                std::cout << "Tracing ray at pixel " << j << ',' << i  << " sample #" << k << std::endl;
+
+                Color3d rayColor = scene.trace_ray(r);
+
+                color += rayColor;
+            }
+
+            color = color / this->_samples_per_pixel;
+
             img(i, j) = Bpixel(color[0] * 255, color[1] * 255, color[2]* 255);
         }
     }
