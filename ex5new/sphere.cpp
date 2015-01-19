4e8e107fb547cd774f095ea9bc6d0e2dacbdeccf
ori-moisis <ori_moisis@hotmail.com>
2015-01-19 13:44:56 +0200

    made sphere texture look a bit better, not perfect though...

--- ex5new/sphere.cpp
+++ ex5new/sphere.cpp
@@ -47,13 +47,18 @@ int Sphere::intersect(Ray& ray, double tMax, double& t, Point3d& P,
     N = P - this->_C;
     N.normalize();
 
+    // Make sure the normal is pointing in the right direction
+    if (OpenMesh::dot(N, ray.D()) > 0) {
+        N = -N;
+    }
+
     texColor = COLOR_WHITE;
     if (this->_diffuseTexture) {
         double theta = atan2(N[0], N[2]);
         double phi = atan2(N[1], sqrt(N[0]*N[0] + N[2]*N[2]));
 
         double texU = CLAMP((theta + M_PI) / (2 * M_PI)) * this->_diffuseTexture->width();
-        double texV = CLAMP((phi + (M_PI_2)) / M_PI) * this->_diffuseTexture->height();
+        double texV = CLAMP((phi + M_PI_2) / M_PI) * this->_diffuseTexture->height();
 
         Bpixel& tex = (*this->_diffuseTexture)(texU, texV);
         texColor[0] = tex.r;
