7c9020685f23105cc4f2ccb9f3c73198f4726c8b
Oren Samuel <oren.samuel@mail.huji.ac.il>
2015-01-21 14:06:36 +0200

    cone works

--- ex5new/cone.cpp
+++ ex5new/cone.cpp
@@ -25,23 +25,33 @@ Cone::~Cone() {
 }
 
 
+static const double myEps = 1e-4;
+
 int Cone::intersect(IN Ray& ray, IN double tMax, OUT double& t, OUT Point3d& P,
 					OUT Vector3d& N, OUT Color3d& texColor) const {
 
+	texColor = COLOR_WHITE;
 	glm::vec3 rayO(toGlm(ray.O()));
 	glm::vec3 rayD(toGlm(ray.D()));
 
 	// Check point on cap
-	double tmpT = glm::dot(-_D, (rayO - _center)) / glm::dot(_D, rayD);
-	if (tmpT > tMax) {
-		return NO_INTERSECTION;
-	}
-	glm::vec3 tmpP(toGlm(ray(tmpT)));
-	if (glm::distance(tmpP, _center) < _r) {
-		t = tmpT;
-		P = toOpenMesh(tmpP);
-		N = toOpenMesh(-_D);
-		return INTERSECTION;
+	//double capT;
+	Vector3d capP;
+	Vector3d capN;
+	bool hitCap = false;
+	bool hitCone = true;
+
+	double capT = -glm::dot(-_D, (rayO - _center)) / glm::dot(-_D, rayD);
+
+	glm::vec3 tmpP(toGlm(ray(capT)));
+	if (glm::distance(tmpP, _center) <= _r && capT < tMax && capT > myEps) {
+		capP = toOpenMesh(tmpP);
+		capN = toOpenMesh(-_D);
+		//std::cout << "HIT CAP" << std::endl;
+		//std::cout << "Normal is (" << capN[0] << ", " << capN[1] << ", " << capN[2] << ")"<< std::endl;
+		//std::cout << "AT POINT " << capP << std::endl;
+
+		hitCap = true;
 	}
 
 	glm::vec3 delta = rayO - this->_V;
@@ -53,58 +63,70 @@ int Cone::intersect(IN Ray& ray, IN double tMax, OUT double& t, OUT Point3d& P,
 	double disc = c1*c1 - c0*c2;
 
 	if (disc < 0) {
-		return NO_INTERSECTION;
+		hitCone = false;
 	}
 
 	disc = sqrt(disc);
 	double sol1 = (-c1 + disc) / (c2);
 	double sol2 = (-c1 - disc) / (c2);
 
-	double minT = std::min(sol1, sol2);
+	double coneT = std::min(sol1, sol2);
 
-	if (minT > tMax) {
-		return NO_INTERSECTION;
+	if (coneT > tMax) {
+		hitCone = false;
 	}
-	if (minT < 0) {
-		minT = std::max(sol1, sol2);
+	if (coneT < myEps) {
+		coneT = std::max(sol1, sol2);
 	}
-	if (minT < 0) {
-		return NO_INTERSECTION;
+	if (coneT < myEps) {
+		hitCone = false;
 	}
 
-	t = minT;
-	P = ray(t);
+	tmpP = toGlm(ray(coneT));
 
-
-//	std::cout << "P is (" << P[0] << ", " << P[1] << ", " << P[2] << ")"<< std::endl;
-//	std::cout << "_V is (" << _V[0] << ", " << _V[1] << ", " << _V[2] << ")"<< std::endl;
-//	std::cout << "_D is (" << _D[0] << ", " << _D[1] << ", " << _D[2] << ")"<< std::endl;
-//	std::cout << "planeD is: " << planeD << std::endl;
-
-	glm::vec3 PG(toGlm(P));
+	//std::cout << "Cone P is " << toOpenMesh(tmpP) << std::endl;
 
 	// Take only lower side of the cone
-	if (glm::dot(_D, PG) > _planeD) {
-		return NO_INTERSECTION;
+	if (glm::dot(_D, tmpP) > _planeD) {
+		hitCone = false;
 	}
 
 	// Limit height
-	double dist = glm::length(_V - PG);
+	double dist = glm::length(_V - tmpP);
 	if (dist > _maxDist)
+	{
+		hitCone = false;
+	}
+
+
+	if ((hitCone && hitCap && capT < coneT) || (hitCap && !hitCone)) {
+		N = capN; P = capP; t = capT;
+		return INTERSECTION;
+	}
+
+	if (!hitCone)
 	{
 		return NO_INTERSECTION;
 	}
 
+	t = coneT;
+	P = toOpenMesh(tmpP);
+
 	// Calculate normal
 	double tant = tan(_theta);
-	Vector3d U(PG.x - _V.x, 0, PG.z - _V.z);
+	Vector3d U(tmpP.x - _V.x, 0, tmpP.z - _V.z);
 	U.normalize();
 	N = Vector3d(U[0]/tant, tant, U[2]/tant);
+//
+//	glm::vec3 NG(2*P[0], 2*P[1], -2*P[2]);
+//	glm::rota
+//	NG = NG - _V;
+//	N = toOpenMesh(NG);
+
 	N.normalize();
 
-//	glm::vec3 NG(2*P[0], 2*P[1], -2*P[2]);
-//	N = toOpenMesh(glm::inverse(_M) * NG);
-//	std::cout << "Normal is (" << N[0] << ", " << N[1] << ", " << N[2] << ")"<< std::endl;
+	std::cout << "t is " << t << std::endl;
+	std::cout << "Normal is (" << N[0] << ", " << N[1] << ", " << N[2] << ")"<< std::endl;
 	return INTERSECTION;
 }
 
